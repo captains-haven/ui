cc593cbcb6e99028519ba3ad1bb99c650d3db487
Captain_Of_Coit <admin@captains-haven.org>
2023-02-12 13:33:33 +0100

    handle errors better

--- src/main/portal/core.cljs
+++ src/main/portal/core.cljs
@@ -2,6 +2,7 @@
     (:require
      [portal.time :refer [simple-datetime]]
      [clojure.pprint :refer [pprint]]
+     [clojure.string :as str]
      [clojure.edn :refer [read-string]]
      [cljs.core.async :refer [go]]
      [cljs.core.async.interop :refer-macros [<p!]]
@@ -445,6 +446,13 @@
     (pprint form-data)
     (post-formdata "upload" form-data)))
 
+(defn slugify
+  [string]
+  ((comp #(str/replace % #" " "-")
+         str/lower-case
+         str/trim)
+   string))
+
 (defn $blueprints-new-page []
   (let [is-loading (r/atom false)
         s (r/atom {:title ""
@@ -456,11 +464,15 @@
                    :author (:id (ls-get "user"))
                    :thumbnail nil
                    :res nil
-                   :upload-res nil})
+                   :upload-res nil
+                   :blueprint-error nil
+                   :error nil})
         upload-res (r/atom nil)
         parse-blueprint-timeout (r/atom nil)
         parse-blueprint (fn [data]
-                          (swap! s assoc :blueprint_data data)
+                          (swap! s assoc
+                                 :blueprint_data data
+                                 :blueprint-error nil)
                           (when @parse-blueprint-timeout
                             (js/window.clearTimeout @parse-blueprint-timeout)
                             (reset! parse-blueprint-timeout nil))
@@ -475,25 +487,42 @@
                                                             :body data}))
                                               parsed (<p! (.json res))
                                               obj (js->clj parsed :keywordize-keys true)]
-                                          (pprint obj)
-                                          (swap! s assoc
-                                                 :title (:name obj)
-                                                 :description (:desc obj)
-                                                 :items_count (:items_count obj)
-                                                 :items (:items obj))
-                                          (reset! is-loading false)))
-                                      (println "hello"))
+                                          (if (:error obj)
+                                            (do
+                                              (swap! s assoc
+                                                     :blueprint-error (:error obj))
+                                              (reset! is-loading false))
+                                            (do
+                                              (pprint obj)
+                                              (swap! s assoc
+                                                     :title (:name obj)
+                                                     :description (:desc obj)
+                                                     :items_count (:items_count obj)
+                                                     :items (:items obj))
+                                              (reset! is-loading false))))))
                                     100)))
         submit-blueprint
         (fn []
-          (reset! is-loading true)
-          (go
-            (println "creating")
-            (let [res (<p! (create-resource "blueprints" @s))]
-              (println "created")
-              (swap! s assoc :res res)
-              (reset! is-loading false)
-              (go-to (str "/blueprints/" (-> res :data :id))))))]
+          (if (nil? (:thumbnail @s))
+            (swap! s assoc :error {:name "Missing thumbnail!" :message "You need to upload a thumbnail to go with your blueprint"})
+            (do
+              (reset! is-loading true)
+              (go
+                (println "creating")
+                (let [to-submit (assoc @s :slug (-> @s :title slugify))
+                      res (<p! (create-resource "blueprints" to-submit))]
+                  (if (:error res)
+                    (do
+                      (if (= (-> res :error :message) "This attribute must be unique")
+                       (swap! s assoc :error {:name "Title not unique" :message "Title has to be unique, seems another blueprint already use the same title, please change it"})
+                        (swap! s assoc :error (:error res)))
+                      (reset! is-loading false))
+                    (do
+                      (pprint res)
+                      (println "created")
+                      (swap! s assoc :res res)
+                      (reset! is-loading false)
+                      (go-to (str "/blueprints/" (-> res :data :attributes :slug))))))))))]
     (r/create-class
     {:reagent-render
      (fn []
@@ -504,6 +533,8 @@
                       :onChange #(parse-blueprint (-> % .-target .-value))
                       :value (:blueprint_data @s)
                       :placeholder "B2416:H4sIAAAAAAAACnVWzW8bxxV...."}]
+        (when (:blueprint-error @s)
+          [:div (:blueprint-error @s)])
         [$text-input {:label "Title"
                       :disabled @is-loading
                       :onChange (handle-text-input-change s :title)
@@ -530,6 +561,7 @@
             :name "file"
             :onChange (fn [ev]
                         (reset! is-loading true)
+                        (swap! s assoc :error nil)
                         (pprint (-> ev .-target .-value))
                         (pprint (upload-file (-> ev .-target)))
                         (go
@@ -541,6 +573,11 @@
           [:img
            {:width 300
             :src (relative->absolute-url image-url)}])
+        (when (:error @s)
+          [:div
+           (-> @s :error :name)
+           " - "
+           (-> @s :error :message)])
         [$btn {:label "Submit"
                :disabled @is-loading
                :onClick submit-blueprint}]
