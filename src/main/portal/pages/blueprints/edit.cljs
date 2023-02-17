bd9f28a1184e41bb6b4e2a17ca8f896b552e6b27
Captain_Of_Coit <admin@captains-haven.org>
2023-02-17 12:31:05 +0100

    fix blueprint edit to new structure

--- src/main/portal/pages/blueprints/edit.cljs
+++ src/main/portal/pages/blueprints/edit.cljs
@@ -1,19 +1,19 @@
 (ns portal.pages.blueprints.edit
   (:require
    [reagent.core :as r]
-   
+
    [cljs.core.async :refer [go]]
    [cljs.core.async.interop :refer-macros [<p!]]
-   
+
    [portal.router :refer [go-to]]
    [portal.http :refer [update-resource
                         fetch-resource-by-slug]]
-   
+
    [portal.form-utils :refer [handle-text-input-change-nested]]
    [portal.components.debug :refer [$debug]]
    [portal.components.file-upload :refer [$file-upload]]
    [portal.components.text-input :refer [$text-input]]
-   [portal.components.blueprint :refer [$blueprint]]))
+   [portal.components.blueprint :refer [$blueprint-new]]))
 
 (defn $blueprints-edit-page [{:keys [slug]}]
   (let [is-loading (r/atom false)
@@ -22,9 +22,8 @@
         (fn []
           (go
             (let [to-submit (-> @item
-                                :attributes
                                 (assoc :thumbnail (or (:new-thumbnail @item)
-                                                      (-> @item :attributes :thumbnail :data :id))))
+                                                      (-> @item :thumbnail :id))))
                   res (<p! (update-resource "blueprints" to-submit (:id @item)))]
               (go-to (str "/blueprints/" (-> res :data :attributes :slug))))))]
     (r/create-class
@@ -32,7 +31,7 @@
       (fn []
         (go
           (let [found-item (<p! (fetch-resource-by-slug "blueprints" slug))]
-            (reset! item (first (:data found-item))))))
+            (reset! item found-item))))
       :reagent-render
       (fn [{:keys [_slug]}]
         [:div
@@ -46,16 +45,16 @@
            "Warning: Blueprint Data nor Title can be updated."]
           [$text-input {:label "Blueprint Data"
                         :disabled true ;; can never update blueprint_data 
-                        :value (-> @item :attributes :blueprint_data)
+                        :value (:blueprint_data @item)
                         :placeholder "B2416:H4sIAAAAAAAACnVWzW8bxxV...."}]
           [$text-input {:label "Title"
                         :disabled true ;; can never update title has we use it for urls 
-                        :value (-> @item :attributes :title)
+                        :value (:title @item)
                         :placeholder "Blueprint Title"}]
           [$text-input {:label "Description"
                         :disabled @is-loading
-                        :onChange (handle-text-input-change-nested item [:attributes :description])
-                        :value (-> @item :attributes :description)
+                        :onChange (handle-text-input-change-nested item [:description])
+                        :value (:description @item)
                         :multiline true
                         :placeholder "Description"}]
           [:label
@@ -66,8 +65,10 @@
                         (reset! is-loading true))
              :onDone (fn [res]
                        (swap! item assoc :new-thumbnail (-> res first :id))
-                       (let [url (-> res first :formats :thumbnail :url)]
-                         (swap! item assoc-in [:attributes :thumbnail :data :attributes :url] url))
+                       (let [full-url (-> res first :url)
+                             thumbnail-url (-> res first :formats :thumbnail :url)]
+                         (swap! item assoc :thumbnail {:url full-url
+                                                       :formats {:thumbnail {:url thumbnail-url}}}))
                        (reset! is-loading false))}]]
           [:div
            {:style {:margin-top 15}}
@@ -77,5 +78,5 @@
          [:div
           {:style {:margin-left 30}}
           [:h2 "Preview"]
-          [$blueprint @item true]]
-         [$debug @item]])})))
+          [$blueprint-new @item true]
+          [$debug @item]]])})))
